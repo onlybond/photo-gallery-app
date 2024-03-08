5c95ce53cfb336e4738abb3074ccf1e868abafdd
onlybond <36098799+onlybond@users.noreply.github.com>
2024-03-08 18:43:25 +0530

    first commit

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    "name": "Photo 1",
+    "url": "/images/1.png",
+    "description": "Photo 1 description",
+    "date": "2020-01-01",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 2",
+    "url": "/images/2.jpg",
+    "description": "Photo 2 description",
+    "date": "2020-01-02",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 3",
+    "url": "/images/3.jpg",
+    "description": "Photo 3 description",
+    "date": "2020-01-03",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 4",
+    "url": "/images/4.jpg",
+    "description": "Photo 4 description",
+    "date": "2020-01-04",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 5",
+    "url": "/images/5.jpg",
+    "description": "Photo 5 description",
+    "date": "2020-01-05",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 6",
+    "url": "/images/6.jpg",
+    "description": "Photo 6 description",
+    "date": "2020-01-06",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 7",
+    "url": "/images/7.jpg",
+    "description": "Photo 7 description",
+    "date": "2020-01-07",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 8",
+    "url": "/images/8.jpg",
+    "description": "Photo 8 description",
+    "date": "2020-01-08",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 9",
+    "url": "/images/9.jpg",
+    "description": "Photo 9 description",
+    "date": "2020-01-09",
+    "type": "image/jpg",
+  }
+]
